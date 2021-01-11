e3a74c967c1b1e85707596800b3988e3703fadfc
seojinsarejj <s19009@naver.com>
2021-01-11 23:30:44 +0900

    [CREATE] Create Dockerfile

--- app/main/service/emotion_service.py
+++ app/main/service/emotion_service.py
@@ -4,7 +4,11 @@ from app.main import db
 from app.main.model.diary import Diary
 from .auth_helper import Auth
 
+from ....manage import session
 
 
+def get_my_emotions(request,year,month):
+    resp = Auth.get_user_id_with_token(request)
 
-
+    result = Diary.query(Diary.id,Diary.emotion,Diary.value).filter(Diary.user_id == resp).all()
+    return result
